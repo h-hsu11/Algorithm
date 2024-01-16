02b2637297422a219f073ab6d5ada1b8ffdbce31
h-hsu11 <149367366+h-hsu11@users.noreply.github.com>
2024-01-17 02:10:14 +0900

    [level Lv.0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/Lv.0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,5 +1,7 @@
 def solution(my_string, s, e):
     return (my_string[:s]+my_string[s:e+1][::-1]+my_string[e+1:])
 
+#왜 my_string[s:e+1:-1]은 안돼??
+#문자열은 reverse 메서드가 없다. 따라서 list로 바꾸어 reverse하고 join해줘야 한다.
 
         
