3655c5aef0c251ae1b10bef8411c7e1158e81d34
h-hsu11 <149367366+h-hsu11@users.noreply.github.com>
2024-01-23 04:30:57 +0900

    [level 0] Title: 5명씩, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.py
+++ 프로그래머스/0/181886. 5명씩/5명씩.py
@@ -4,3 +4,25 @@ def solution(names):
         if i%5==0:
             answer.append(names[i])
     return answer
+
+'''
+enumerate 함수 쓰기
+
+
+def solution(names):
+    answer = []
+    for i,str in enumerate(range(0,len(names),5))  -- range로 특정 간격의 인덱스 형성, enumerate 필요없음. 
+        answer.append(names[i])
+    return answer
+
+
+수정코드:
+
+def solution(names):
+    answer = []
+    for idx, name in enumerate(names[::5]):    
+        answer.append(name)
+    return answer
+    
+하지만 직접 5단계마다 리스트에서 가져오는 것이 메모리 사용상 효율적
+'''
